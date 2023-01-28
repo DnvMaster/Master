a3401a5c6fffda4af203ab849eaa9b914fccf64c
DnvMaster <dnvcomp@yandex.ru>
2023-01-28 13:03:00 +0200

    130228012023 Menu navigation

--- resources/views/master/navigation.blade.php
+++ resources/views/master/navigation.blade.php
@@ -1,21 +1,45 @@
 <!-- Start navigation -->
-<nav class="navbar navbar-default">
-    <div class="container">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+<nav class="navbar navbar-default navbar-top">
+    <div class="container-fluid">
+        <div class="navbar-header head-nav">
+            <button type="button" class="navbar-toggle bar-icon" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
                 <span class="sr-only">Toggle navigation</span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-logo" href="{{ url('/') }}">
-                <img src="{{ asset(env('MASTER')) }}/images/dnvmaster-1.png" alt="DnvMaster">
-            </a>
+            <a class="navbar-logo" href="#"><img src="{{ asset(env('MASTER')) }}/images/dnvmaster-1.png" alt="DnvMaster"></a>
         </div>
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav">
-                <li class="active"><a href="{{ url('/') }}"><i class="fa fa-home"></i> Главная</a></li>
-                <li><a href="{{ url('/') }}"><i class="fa fa-edit"></i> О нас</a></li>
+            <ul class="nav navbar-nav nav-links">
+                <li><a href="#">Главная</a></li>
+                <li><a href="#">О нас</a></li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Категория <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Сборка мебели</a></li>
+                        <li><a href="#">Услуги грузчиков</a></li>
+                        <li><a href="#">Ремонт компьютеров</a></li>
+                        <li><a href="#">Уборка квартир</a></li>
+                        <li><a href="#">Электромонтеры</a></li>
+                    </ul>
+                </li>
+            </ul>
+            <form class="navbar-form navbar-left nav-search">
+                <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Search">
+                </div>
+                <button type="submit" class="btn btn-default">Submit</button>
+            </form>
+            <ul class="nav navbar-nav nav-links">
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Отзывы <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Марина Васильевна</a></li>
+                        <li><a href="#">Виктор Степанович</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Контакты</a></li>
             </ul>
         </div>
     </div>
